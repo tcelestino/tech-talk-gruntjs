1f03c5d30ce30371507faa84aca9a255d78b047f
Tiago Celestino <tcelestino@gmail.com>
2014-06-12 20:35:35 -0300

    remove function test

--- spec/main.Spec.js
+++ spec/main.Spec.js
@@ -16,18 +16,12 @@ describe("Stock Portfolio App Tests", function() {
   });
    
   it("calcWeeklyPercentChange() should return the change between two numbers as a percentage.", function() {
-    var calcWeeklyPercentChange = function(presentValue, previousValue, aPercentChanges) {
-      var percentChange = presentValue / previousValue - 1;
-      aPercentChanges.push(percentChange);
-      return percentChange;
-    };
- 
     var result = calcWeeklyPercentChange(presentValue, previousValue, aPercentChanges);
     expect(result).toBeCloseTo(0.1);
     expect(aPercentChanges.length).toEqual(1);
   });
    
   it("The aPercentChanges array should now be empty.", function() {
-    expect(aPercentChanges.length).toEqual(1);
+    expect(aPercentChanges.length).toEqual(0);
   });
 });
